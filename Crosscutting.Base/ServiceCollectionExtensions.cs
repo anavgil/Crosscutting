1b9d4aa729d0595d620250431cd824e0eae215d5
Alberto Navarro Gil <a.navarro@innovation.es>
2023-08-02 10:03:23 +0200

    Refactor methods extension

---
+++ Crosscutting.Base/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Crosscutting.Base;
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection ConfigureBase(this IServiceCollection services)
+    {
+
+        services.AddSerilog();
+        return services;
+    }
+
+
+}

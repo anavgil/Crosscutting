e37d943a93e02a117b488aab978dc4f63c6f9b1c
Alberto Navarro Gil <a.navarro@innovation.es>
2023-08-03 15:10:47 +0200

    Added cache service

---
+++ Crosscutting.Cache/Abstraction/ICacheService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+
+namespace Crosscutting.Cache.Abstraction;
+
+public interface ICacheService
+{
+    Task SetAsync<T>(string key, T value, DistributedCacheEntryOptions options);
+    void Set<T>(string key, T value, DistributedCacheEntryOptions options);
+    Task<T> TryGetValueAsync<T>(string key, CancellationToken ct = default);
+    bool TryGetValue<T>(string key, out T value);
+    void RemoveKey(string key);
+    Task RemoveKeyAsync(string key);
+}

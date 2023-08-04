2cc7368cfdff0df9af6d94587bf72c5d37f644aa
Alberto Navarro Gil <a.navarro@innovation.es>
2023-08-04 13:16:00 +0200

    Added Repository and UOW to persistence Folder solution refactor Added Enumeration abstract class and test class

---
+++ Crosscutting.Base/Enums/TestEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace Crosscutting.Base.Enums
+{
+    internal class TestEnum : Enumeration
+    {
+        public static TestEnum Tipo1 = new(1, nameof(Tipo1));
+        public static TestEnum Tipo2 = new(2, nameof(Tipo2));
+        public static TestEnum Tipe3 = new(3, nameof(Tipe3));
+
+        public TestEnum(int id, string name) : base(id, name)
+        {
+        }
+    }
+}
---
+++ Crosscutting.Persistence/DependencyInjection/ServiceCollectionExtebsions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Threenine.Data.DependencyInjection;
+
+namespace Crosscutting.Persistence.DependencyInjection
+{
+    public static class ServiceCollectionExtebsions
+    {
+        public static IServiceCollection ConfigurePersistenceLayer<TContext>(this IServiceCollection services) where TContext : DbContext
+        {
+
+            //https://genericrepository.readthedocs.io/en/latest/getting-started/
+
+
+            services.AddUnitOfWork<TContext>();
+            return services;
+        }
+    }
+}

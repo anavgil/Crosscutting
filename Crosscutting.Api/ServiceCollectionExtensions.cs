f81296d7afb72a8e87bdf18ed7f780e514259fe0
Alejandro Gelabert <a.gelabert@innovation.es>
2023-08-07 15:47:08 +0200

    Add AutoMapper

--- Crosscutting.Api/ServiceCollectionExtensions.cs
+++ Crosscutting.Api/ServiceCollectionExtensions.cs
@@ -1,10 +1,20 @@
 ﻿using Croscutting.Common.Configurations.Exception;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Crosscutting.Api
 {
-    public static  class ServiceCollectionExtensions
+    public static  class ServiceCollectionExtensions 
     {
+        public static IServiceCollection ConfigureBase(this IServiceCollection services)
+        {
+            services.ConfigureSettings();
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+
         public static IServiceCollection ConfigureSettings(this IServiceCollection services)
         {
             services.ConfigureOptions<ExceptionSettingsBinder>();
--- Crosscutting.Persistence/DependencyInjection/ServiceCollectionExtensions.cs
+++ Crosscutting.Persistence/DependencyInjection/ServiceCollectionExtensions.cs
@@ -4,15 +4,13 @@ using Threenine.Data.DependencyInjection;
 
 namespace Crosscutting.Persistence.DependencyInjection
 {
-    public static class ServiceCollectionExtebsions
+    public static class ServiceCollectionExtensions
     {
         public static IServiceCollection ConfigurePersistenceLayer<TContext>(this IServiceCollection services) where TContext : DbContext
         {
-
             //https://genericrepository.readthedocs.io/en/latest/getting-started/
-
-
             services.AddUnitOfWork<TContext>();
+
             return services;
         }
     }

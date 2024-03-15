bc7010d1bb38b2dac222c75ec8fb46648efea39a
Alberto Navarro Gil <a.navarro@innovation.es>
2024-03-15 11:49:03 +0100

    Fix ApiResponseBase

--- Crosscutting.Base/Messages/Base/ApiResponseBase.cs
+++ Crosscutting.Base/Messages/Base/ApiResponseBase.cs
@@ -1,10 +1,33 @@
 ﻿namespace Crosscutting.Application.Messages.Base;
 
-public record ApiResponseBase<T> where T : class?
+public interface IApiResponseBase
+{
+    bool IsSuccess { get; set; }
+    List<ApiError>? ErrorMessages { get; set; }
+}
+
+public interface IApiResponseBaseReadOnly<T> where T : class?
+{
+    T? Data { get; set; }
+}
+
+public interface IApiResponseBaseScalar<T> where T : struct
+{
+    T? Data { get; set; }
+}
+
+public record ApiResponseBase<T> : IApiResponseBase, IApiResponseBaseReadOnly<T> where T : class?
+{
+    public bool IsSuccess { get;  set; }
+    public List<ApiError>? ErrorMessages { get;  set; }
+    public T? Data { get; set; }
+}
+
+public record ApiResponseBaseScalar<T> : IApiResponseBase, IApiResponseBaseScalar<T> where T : struct
 {
     public bool IsSuccess { get; set; }
-    public T? Data { get; set; }
     public List<ApiError>? ErrorMessages { get; set; }
+    public T? Data { get; set; }
 }
 
 public record ApiError(string Code, string Description){}
--- Crosscutting.Base/Messages/Base/ApiResponseExtensions.cs
+++ Crosscutting.Base/Messages/Base/ApiResponseExtensions.cs
@@ -26,4 +26,29 @@ public static class ApiResponseExtensions
 
         return responseBase;
     }
+
+    public static ApiResponseBaseScalar<T> ToApiResponseBaseScalar<T>(this FluentResults.IResult<T> result) where T : struct
+    {
+        ApiResponseBaseScalar<T> responseBase = new()
+        {
+            IsSuccess = result.IsSuccess,
+            Data = result.ValueOrDefault,
+            ErrorMessages = []
+        };
+
+        if (result.IsFailed && result.Errors.Count != 0)
+        {
+            foreach (var error in result.Errors)
+            {
+                responseBase.ErrorMessages.Add(new ApiError(string.Empty, error.Message));
+
+                foreach (var reason in error.Reasons)
+                {
+                    responseBase.ErrorMessages.Add(new ApiError(string.Empty, reason.Message));
+                }
+            }
+        }
+
+        return responseBase;
+    }
 }

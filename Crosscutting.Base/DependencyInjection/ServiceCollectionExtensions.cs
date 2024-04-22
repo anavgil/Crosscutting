70662aceb1557954d817f6f53dd9d1a4b0b9df58
Alberto Navarro Gil <a.navarro@innovation.es>
2024-04-22 12:25:13 +0200

    Refactor AddApplicationCrosscutting

--- Crosscutting.Base/DependencyInjection/ServiceCollectionExtensions.cs
+++ Crosscutting.Base/DependencyInjection/ServiceCollectionExtensions.cs
@@ -6,30 +6,11 @@ using System.Reflection;
 namespace Crosscutting.Base.DependencyInjection;
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddApplicationCrosscutting(this IServiceCollection services)
+    public static IServiceCollection AddApplicationCrosscutting(this IServiceCollection services, Assembly configurationAssembly)
     {
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly())
-                .AddMapsterConfiguration()
+                .AddMapsterConfiguration(configurationAssembly)
                 .AddMapster();
-
-        return services;
-    }
-
-
-    private static IServiceCollection AddMapsterConfiguration(this IServiceCollection services)
-    {
-        var config = TypeAdapterConfig.GlobalSettings;
-
-        //var assembly = Assembly.GetExecutingAssembly();
-        var assembly = typeof(ServiceCollectionExtensions).Assembly;
-
-        //var applicationAssembly = assembly!.GetReferencedAssemblies()
-        //                            .Where(x => x.FullName.Contains("Application"))
-        //                            .FirstOrDefault();
-
-        if (assembly is not null)
-            config.Scan(assembly);
-
 
         return services;
     }
@@ -40,25 +21,6 @@ public static class ServiceCollectionExtensions
 
         if (configurationAssembly is not null)
             config.Scan(configurationAssembly);
-
-        return services;
-    }
-
-    private static IServiceCollection AddMapsterConfiguration(this IServiceCollection services, string configurationAssemblyName)
-    {
-        var config = TypeAdapterConfig.GlobalSettings;
-
-        if (!string.IsNullOrWhiteSpace(configurationAssemblyName))
-        {
-            var assembly = Assembly.GetEntryAssembly();
-            var applicationAssembly = assembly!.GetReferencedAssemblies()
-                                        .Where(x => x.FullName.Contains(configurationAssemblyName))
-                                        .FirstOrDefault();
-
-            if (applicationAssembly is not null)
-                config.Scan(Assembly.Load(applicationAssembly));
-        }
-
 
         return services;
     }

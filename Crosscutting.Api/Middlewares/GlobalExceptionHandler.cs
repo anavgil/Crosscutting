589ccbbae9dfded99a02e2547b7df0977fe1dd5c
Alberto Navarro Gil <a.navarro@innovation.es>
2024-09-20 13:53:04 +0200

    New fix on validation

--- Crosscutting.Api/Middlewares/GlobalExceptionHandler.cs
+++ Crosscutting.Api/Middlewares/GlobalExceptionHandler.cs
@@ -8,6 +8,10 @@ namespace Crosscutting.Api.Middlewares;
 
 public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> _logger) : IExceptionHandler
 {
+    private const string validationExceptionTitle = "One or more validation errors occurred.";
+    private const string validationExceptionType = "https://tools.ietf.org/html/rfc7231#section-6.5.1";
+    private const string standarExceptionTitle = "One error occurred.";
+
     public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
     {
         var exceptionMessage = exception.Message;
@@ -25,12 +29,29 @@ public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> _logger) : I
             Instance = httpContext.Request.Path
         };
 
-        if (exception.InnerException is not null)
-            problemDetails.Extensions = new Dictionary<string, object>()
+        if (exception is CustomValidationException fluentException)
+        {
+            problemDetails.Title = validationExceptionTitle;
+            problemDetails.Type = validationExceptionType;
+            List<string> validationErrors = [];
+            foreach (var error in fluentException.Errors)
+            {
+                validationErrors.Add(error.Error);
+            }
+            problemDetails.Extensions.Add("errors", validationErrors);
+        }
+        else
+        {
+            problemDetails.Title = standarExceptionTitle;
+            problemDetails.Type = exception.GetType().Name;
+
+            if (exception.InnerException is not null)
+                problemDetails.Extensions = new Dictionary<string, object>()
             {
                 { "INNER-Message",exception.InnerException.Message },
                 { "INNER-Type",exception.InnerException.GetType().Name}
             };
+        }
 
 
         httpContext.Response.StatusCode = problemDetails.Status.Value;
@@ -49,3 +70,4 @@ public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> _logger) : I
         _ => StatusCodes.Status500InternalServerError
     };
 }
+    

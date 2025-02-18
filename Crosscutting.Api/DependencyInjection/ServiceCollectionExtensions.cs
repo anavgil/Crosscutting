01576a5f4f0a621ad31e9ff24cf196b51a5745dd
Alberto Navarro Gil <a.navarro@innovation.es>
2025-02-18 08:59:11 +0100

    Nuget update

--- Crosscutting.Api/DependencyInjection/ServiceCollectionExtensions.cs
+++ Crosscutting.Api/DependencyInjection/ServiceCollectionExtensions.cs
@@ -112,12 +112,11 @@ public static class ServiceCollectionExtensions
 
     public static IServiceCollection AddEndpoints(this IServiceCollection services, Assembly assembly)
     {
-        ServiceDescriptor[] serviceDescriptors = assembly
+        ServiceDescriptor[] serviceDescriptors = [.. assembly
                                                 .DefinedTypes
                                                 .Where(type => type is { IsAbstract: false, IsInterface: false } &&
                                                                type.IsAssignableTo(typeof(IEndpoint)))
-                                                .Select(type => ServiceDescriptor.Transient(typeof(IEndpoint), type))
-                                                .ToArray();
+                                                .Select(type => ServiceDescriptor.Transient(typeof(IEndpoint), type))];
 
         services.TryAddEnumerable(serviceDescriptors);
 
